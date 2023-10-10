a6a0ec7d0a929cfe2aae77259908229870326281
kishore sm <kishoresm.20@gmail.com>
2023-10-10 09:42:18 +0530

    pushing to master

---
+++ src/main/java/com/learn/gitversioning/GitVersioningApplication.java
@@ -0,0 +1,15 @@
+package com.learn.gitversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitVersioningApplication.class, args);
+	}
+
+}
+
+
